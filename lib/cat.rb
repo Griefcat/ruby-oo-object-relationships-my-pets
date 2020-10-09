f9dfb8818b293c55242178c708bbdae546eb1c50
Bryce Moore <brycemooore@gmail.com>
2020-10-09 13:05:15 -0400

    Finshed cat and dog classes

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,6 +1,20 @@
 class Cat
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
   
-  def initialize
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    self.class.all << self
+  end 
+
+  def self.all
+    @@all
+  end 
 
 
 
--- lib/owner.rb
+++ lib/owner.rb
@@ -14,6 +14,20 @@ def say_species
   "I am a #{self.species}."
 end 
 
+def cats
+  Cat.all.select do |cat|
+    cat.owner == self
+  end
+end
+
+def dogs
+  Dog.all.select do |dog|
+    dog.owner == self
+  end
+end 
+
+
+
 def self.all
   @@all
   
