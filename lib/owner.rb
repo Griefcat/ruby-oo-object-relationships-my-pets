65ec8bd753ccb3d5bac38fea509ff411a5a9e792
Griefcat <anardolilli@gmail.com>
2020-10-09 12:39:47 -0400

    passing this off to bryce

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
+
+@@all = []
+
+attr_reader :name, :species
+
+def initialize(name)
+  @name = name
+  @species = "human"
+  self.class.all << self 
+end 
+
+def say_species
+  "I am a #{self.species}."
+end 
+
+def self.all
+  @@all
+  
 end
+
+def self.count
+  self.all.count
+end 
+
+def self.reset_all
+  self.all.clear
+end 
+
+end 
